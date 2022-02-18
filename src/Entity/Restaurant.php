6f45090e328c05193b9cf71dcd9c8a045d152ddc
Guillaume <g.morlieras@gmail.com>
2022-02-18 11:04:23 +0100

    Entité Restaurant

---
+++ src/Entity/Restaurant.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RestaurantRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RestaurantRepository::class)]
+class Restaurant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $nom;
+
+    #[ORM\Column(type: 'string', length: 20)]
+    private $tel;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $email;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $ligne1;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $ligne2;
+
+    #[ORM\Column(type: 'string', length: 70)]
+    private $ville;
+
+    #[ORM\Column(type: 'string', length: 20)]
+    private $codePostal;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getTel(): ?string
+    {
+        return $this->tel;
+    }
+
+    public function setTel(string $tel): self
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getLigne1(): ?string
+    {
+        return $this->ligne1;
+    }
+
+    public function setLigne1(string $ligne1): self
+    {
+        $this->ligne1 = $ligne1;
+
+        return $this;
+    }
+
+    public function getLigne2(): ?string
+    {
+        return $this->ligne2;
+    }
+
+    public function setLigne2(string $ligne2): self
+    {
+        $this->ligne2 = $ligne2;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getCodePostal(): ?string
+    {
+        return $this->codePostal;
+    }
+
+    public function setCodePostal(string $codePostal): self
+    {
+        $this->codePostal = $codePostal;
+
+        return $this;
+    }
+}
